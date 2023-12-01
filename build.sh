82b07b5127070f29bce175140c9e3ca5b69c9464
James Wilmot <jameswilmot2000@gmail.com>
2023-12-01 17:12:22 +1100

    Renamed main document; changed script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 
-typst c --font-path='./assets/fonts/inconsolata' --font-path='./assets/fonts/notosans' --font-path='./assets/fonts/fontawesome' --font-path='./assets/fonts/cerebrisans' main.typ
+typst c --font-path='./assets/fonts/inconsolata' --font-path='./assets/fonts/notosans' --font-path='./assets/fonts/fontawesome' --font-path='./assets/fonts/cerebrisans' ACCSFinalReportWhitepaper.typ
